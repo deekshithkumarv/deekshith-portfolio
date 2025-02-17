553e62992b1d522201a9fbbedf1b0df31039c6d1
vdeekshithkumar <DeekshithK@ivoyant.com>
2025-02-17 19:09:41 +0530

    Initial commit

---
+++ app/components/Header.tsx
@@ -0,0 +1,14 @@
+import Logo from "./Logo";
+import Nav from "./Nav";
+
+export const Header = () => {
+  return (
+    <header className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900 sticky top-0 z-[1] mx-auto  flex w-full flex-wrap items-center justify-between bg-background font-burtons text-xl dark:border-gray-800 dark:bg-d-background ">
+      {/* <Logo /> */}
+      <h1 className="font-burtons text-xl">Deekshith Kumar V</h1>
+      <Nav />
+    </header>
+  );
+};
+
+export default Header;
